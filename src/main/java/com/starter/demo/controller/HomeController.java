9bda9c6d6a54b5bd6d0f0cfdd1e7069bbd84407a
sujoy99 <scg.sujoy@gmail.com>
2022-05-12 17:26:05 +0600

    init

---
+++ src/main/java/com/starter/demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.starter.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class HomeController {
+
+    @GetMapping("/hi")
+    public String home() {
+        return "hi";
+    }
+}

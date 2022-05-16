3dc876abfef4d80b9d328e0710d76b200b6e09aa
sujoy99 <scg.sujoy@gmail.com>
2022-05-16 14:14:00 +0600

    model design

---
+++ src/main/java/com/starter/demo/model/Role.java
@@ -0,0 +1,38 @@
+package com.starter.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TBL_ROLE")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ROLE_ID")
+    private Long id;
+    @Column(name = "NAME")
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

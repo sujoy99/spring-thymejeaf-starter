cab8aeb37abb38ea50ce0036f85d70386651586a
sujoy99 <scg.sujoy@gmail.com>
2022-05-17 16:21:44 +0600

    homepage implementation

--- src/main/java/com/starter/demo/config/SecurityConfiguration.java
+++ src/main/java/com/starter/demo/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -39,11 +40,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(
-                        "/registration",
-                        "/js/**",
-                        "/css/**",
-                        "img/**").permitAll()
+        http.authorizeRequests()
+                .antMatchers("/registration")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -56,5 +55,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login?logout")
                 .permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/resources/**", "/static/**");
     }
 }
--- src/main/java/com/starter/demo/controller/MainController.java
+++ src/main/java/com/starter/demo/controller/MainController.java
@@ -2,6 +2,7 @@ package com.starter.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -9,5 +10,9 @@ public class MainController {
     @GetMapping("/login")
     public String login(){
         return "login";
+    }
+    @GetMapping("/")
+    public String home(){
+        return "index";
     }
 }

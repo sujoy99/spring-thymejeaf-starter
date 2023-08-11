16b66d286c8b8acd56024bd7595bda95dab68f3b
sujoy99 <scg.sujoy@gmail.com>
2023-08-11 13:46:01 +0600

    daily basis log file mechanism add

--- src/main/java/com/starter/demo/controller/HomeController.java
+++ src/main/java/com/starter/demo/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.starter.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class HomeController {
+    private static final Logger LOG =   LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping("/hi")
     public String home() {
+        LOG.info("SJ :: ---GET Controller---");
+        LOG.warn("SJ :: -- WARN");
+        LOG.trace("SJ :: -- TRACE");
+        LOG.error("SJ :: -- ERROR");
+        LOG.debug("SJ :: -- DEBUG");
         return "hi";
     }
 }
